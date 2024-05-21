5b92f4138186297ebdd2320bb0fb9ad9ac52568d
hung311 <87524297+hung311@users.noreply.github.com>
2024-05-21 18:54:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'dương à!',
+    introTitle: 'NHI à!',
     introDesc: `ngồi nói chuyện với tớ 1 tí nha 
-    cậu mà thoát là cậu đồng ý làm vợ tớ đấy
+    cậu mà thoát là cậu đồng ý cậu nguu hơn chó  đấy
     .không mất nhiều thời gian của cậu đâu`,
     btnIntro: 'hihi',
     title: 'tớ muốn nói là😙',
-    desc: 'tớ thích cậu. Cậu có thể cho tớ 1 cơ hội không',
+    desc: 'cậu có ngu và đần không',
     btnYes: 'được chứ, <33',
     btnNo: 'cút, :3',
     question:'thật chứ <3',
     btnReply: 'Gửi cho tớ <3',
     reply: 'thật<3',
-    mess: 'tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'mình ib tiếp nha😍 .',
+    mess: 'tớ biết mà 🥰. Cậu nguu lắm 😘😘',
+    messDesc: 'ib chửi mình đi😍 .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com/bongcute.nhoccon' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com/yeuhung.2k4' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
